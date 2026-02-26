d2c725de17d06f392a566ba40d83b534a50fe847
Hugo <1hugo4lopez@gmail.com>
2026-02-26 12:55:55 +0100

    fix: correct import path for AppContext in Newsletter

--- assets/react/components/home/Newsletter.jsx
+++ assets/react/components/home/Newsletter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useApp } from '../context/AppContext';
+import { useApp } from '../../context/AppContext';
 
 const Newsletter = () => {
     const { user } = useApp();
